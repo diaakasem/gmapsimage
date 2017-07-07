8b13ba70ef1f6951473b2cb2a9689b1a6ea8f60d
PI Home <pi@diaa.me>
2017-07-07 15:05:52 +0000

    updating index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var url = 'https://www.google.com.eg/maps/@30.0237469,31.5255044,879m/data=!3m1!
 casper.start(url, function() {
     this.wait(5000, function() {
     var d = new Date();
-    casper.capture('/media/HDD/apt/home-' + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()  + '.png', {
+    casper.capture('/media/HDD/Pictures/home-' + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()  + '.jpg', {
         top: 0,
         left: 0,
         width: 1280,
