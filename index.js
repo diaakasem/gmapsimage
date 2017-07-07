84a13a297b83af6993b18c66fa84c9cae5f8185f
PI Home <pi@diaa.me>
2017-07-07 15:12:20 +0000

    Adding 0 padding

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ var casper = require('casper').create({
 var url = 'https://www.google.com.eg/maps/@30.0237469,31.5255044,879m/data=!3m1!1e3?hl=en&force=lite';
 casper.start(url, function() {
     this.wait(5000, function() {
-    var d = new Date();
-    casper.capture('/media/HDD/Pictures/home-' + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()  + '.jpg', {
-        top: 0,
-        left: 0,
-        width: 1280,
-        height: 800
-    });
+        var d = new Date();
+        var year = d.getFullYear();
+        var month = d.getMonth() + 1;
+        if (month < 10) {
+           month = '0' + month;
+        }
+        var day = d.getDate();
+        if (day < 10) {
+           day = '0' + day;
+        }
+        casper.capture('/media/HDD/Pictures/home-' + year + "-" + month + "-" + day + '.jpg', {
+            top: 0,
+            left: 0,
+            width: 1280,
+            height: 800
+        });
     });
 });
 
